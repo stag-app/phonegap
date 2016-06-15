6824f10bab02e9a7c277d4d1201334655ad7213c
Bart Langelaan <langelaanbart@gmail.com>
2016-06-15 05:44:02 +0200

    Add software buttons if no NFC or gyro available

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,52 @@
 var count = 0;
 
+var minDeg = 60;
+var minVer = 10;
+
+var $nfcBtn = $('#nfc');
+var $gyroBtn = $('#gyro');
+
 document.addEventListener('deviceready', function(){
-    $('#deviceready').find('.event').toggle();
+    $nfcBtn.click(onTagDiscovered);
+    $gyroBtn.click(onOpen);
+
     if(nfc && nfc.enabled){
         nfc.enabled(function(){
-            $("#nfc").find('.event').toggle();
             nfc.addTagDiscoveredListener(onTagDiscovered);
+            $nfcBtn.hide();
         }, function(){
             nfc.showSettings();
         });
     }
+
+    if(window.DeviceOrientationEvent){
+        window.addEventListener('deviceorientation', orientationCallback, false);
+    }
+
 }, false);
 
 function onTagDiscovered(){
+    count=0;
+    $("h1").text(count);
+}
+
+function onOpen(){
     count++;
     $("h1").text(count);
+}
+
+var open = 0;
+
+function orientationCallback(event){
+    var beta = event.beta;
+
+    if(beta) $gyroBtn.hide();
+
+    if(open != 1 && event.beta > minDeg + minVer){
+        open = 1;
+        onOpen();
+    }
+    else if(open != 0 && event.beta < minDeg){
+        open = 0;
+    }
 }
