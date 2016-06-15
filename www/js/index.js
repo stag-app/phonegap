7de663a4545062c3c954152693a4da8488d90d00
Bart Langelaan <langelaanbart@gmail.com>
2016-06-15 05:07:11 +0200

    Count the numer of ncf hits

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,18 @@
+var count = 0;
+
 document.addEventListener('deviceready', function(){
     $('#deviceready').find('.event').toggle();
     if(nfc && nfc.enabled){
         nfc.enabled(function(){
             $("#nfc").find('.event').toggle();
+            nfc.addTagDiscoveredListener(onTagDiscovered);
         }, function(){
             nfc.showSettings();
         });
     }
 }, false);
+
+function onTagDiscovered(){
+    count++;
+    $("h1").text(count);
+}
